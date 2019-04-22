96c909d9e47653ac037b986303df1cf224f817a9
Nhat-Dang Nguyen <ndang.nguyen@hotmail.com>
2019-04-22 09:19:19 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,12 @@
-def my_select(collection)
- i = 0
- select = []
- while i < collection.lenght
-  if yield (collection[i])
-   select << collection[i]
+def my_select(array)
+  i = 0
+  select = []
+  while i < array.length
+    if yield(array[i])
+      select << array[i]
+    end
+    i+=1
   end
-  i += 1
- end
- select
+  select
 end
+
