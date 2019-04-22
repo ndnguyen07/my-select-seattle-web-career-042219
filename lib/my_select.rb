e72499ce6d9f48d65d133b6480203d82818fe3f7
Nhat-Dang Nguyen <ndang.nguyen@hotmail.com>
2019-04-22 09:16:04 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,8 +2,8 @@ def my_select(collection)
  i = 0
  select = []
  while i < collection.lenght
- if yield (collection[i])
-   select.push collection
+  if yield (collection[i])
+   select.push collection[i]
  end
  1
 end
